ac0374a25bfb9bd6511786ae78d852030937e5b5
PrathmeshModhe9 <lastry2001@gmail.com>
2023-06-21 13:04:57 +0530

    signout func is not able to pass as prop

--- src/components/dashboard/DashBoardToggle.js
+++ src/components/dashboard/DashBoardToggle.js
@@ -1,19 +1,27 @@
-import React from 'react'
-import { Button,Drawer,Icon } from 'rsuite'
+import React, { useCallback } from 'react'
+import { Button,Drawer,Icon,Alert } from 'rsuite'
 import { useMediaQuery, useModalState } from '../../misc/custom-hooks'
 import  Dashboard  from '.';
+import { auth } from '../../misc/firebase';
 
 
 const DashBoardToggle = () => {
   const {isOpen,close,open}=useModalState();
   const isMobile=useMediaQuery('(max-width:992px)');
+
+  const onSignOut=useCallback(()=>{
+    console.log("inside")
+    auth.signOut();
+    Alert.info('Signed out from device',4000);
+    close();
+  },[close])
   return (
     <>
         <Button block color="blue" onClick={open}>
-            <Icon icon="dashboard"/>DashBoard
+            <Icon icon="dashboard"/>Profile
         </Button>
         <Drawer full ={isMobile} show={isOpen} onHide={close} placement='left'>
-               <Dashboard/>
+               <Dashboard OnSignOut={onSignOut}/>
         </Drawer>
 
     </>
--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Drawer,Button } from 'rsuite';
+import { useProfile } from '../../contexts/profile.context';
 
-const DashBoard = () => {
+const DashBoard = ({onSignOut}) => {
+  const {profile}=useProfile();
   return (
-    <div>
-      dashboard is here
-    </div>
+    <>
+    <Drawer.Header>
+         <Drawer.Title>
+           Dashboard
+         </Drawer.Title>
+    </Drawer.Header>
+    <Drawer.Body>
+    {/* {console.log(profile.name)} */}
+       <h3>Hi , {profile.name}</h3>
+    </Drawer.Body>
+    <Drawer.Footer>
+      <Button block color='red' onClick={onSignOut} >SignOut</Button>
+    </Drawer.Footer>
+    </>
+
   )
 }
 
