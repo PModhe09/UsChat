c034c6193d3dda0debb474746dd4b35586e31815
PrathmeshModhe9 <lastry2001@gmail.com>
2023-06-22 13:34:05 +0530

    added nickname input inside profile and updated in db

---
+++ src/components/EditableInput.js
@@ -0,0 +1,43 @@
+import React, { useCallback, useState } from 'react'
+import { Input,Icon,InputGroup, Alert} from 'rsuite'
+
+const EditableInput = ({initalValue,onSave,label=null,placeholder="Your NickName ?",emptyMsg="Input is empty",...InputProps}) => {
+    const [input,setInput]=useState(initalValue);
+    const [isEditable,setIsEditable]=useState(false);
+    const onInputChange=useCallback((value)=>{
+        setInput(value);
+    },[]);
+    const onEditClick=useCallback(()=>{
+        setIsEditable(p=>!p);
+        setInput(initalValue);
+    },[initalValue])
+    const onSaveClick= async()=>{
+        const trimmed=input.trim();
+        if(trimmed===''){
+            Alert.info(emptyMsg,4000)
+        }
+        if(trimmed!=initalValue){
+            await onSave(trimmed);
+        }
+    }
+  return (
+    <div>
+      {label}
+      <InputGroup>
+      <Input {...InputProps} placeholder={placeholder} value={input} onChange={onInputChange} disabled={!isEditable}/>
+      <InputGroup.Button onClick={onEditClick}>
+        <Icon icon={isEditable?'close' : 'edit2'}/>
+      </InputGroup.Button>
+      {isEditable && (
+        <InputGroup.Button onClick={onSaveClick}>
+        <Icon icon="check"/>
+      </InputGroup.Button>
+      )}
+      </InputGroup>
+
+      {input}
+    </div>
+  )
+}
+
+export default EditableInput
--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
-import { Drawer,Button } from 'rsuite';
+import { Drawer,Button, Divider ,Alert} from 'rsuite';
 import { useProfile } from '../../contexts/profile.context';
+import EditableInput from '../EditableInput';
+import { database } from '../../misc/firebase';
 
 const DashBoard = ({onSignOut}) => {
   const {profile}=useProfile();
+
+  const onSave=async newData=>{
+     console.log(newData);
+     const userNicknameRef=database.ref(`/profiles/${profile.uid}`).child('name');
+     try {
+      await userNicknameRef.set(newData);
+      Alert.success('Nickname has been updated',4000);
+     } catch (err) {
+         Alert.error(err.message,4000);
+     }
+  };
   return (
     <>
     <Drawer.Header>
@@ -14,6 +27,13 @@ const DashBoard = ({onSignOut}) => {
     <Drawer.Body>
     {/* {console.log(profile.name)} */}
        <h3>Hi , {profile.name}</h3>
+       <Divider/>
+       <EditableInput
+        name="nickname"
+        initialValue={profile.name}
+        onSave={onSave}
+        label={<h6 className='mb-2'>NickName</h6>}
+       />
     </Drawer.Body>
     <Drawer.Footer>
       <Button block color='red' onClick={onSignOut} >SignOut</Button>
