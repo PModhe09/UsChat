8bf3f35058813942debf7e59bcc582a732eb0136
PrathmeshModhe9 <lastry2001@gmail.com>
2023-06-22 23:21:09 +0530

    added chat button functionality

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import DashBoardToggle from './dashboard/DashBoardToggle'
+import CreateRoomBtnModal from './CreateRoomBtnModal'
 
 const Sidebar = () => {
   return (
     <div>
        <div className='h-100 pt-2'>
-        <div><DashBoardToggle/></div>
+        <div><DashBoardToggle/><CreateRoomBtnModal/></div>
+
        </div>
        bottom
     </div>
