9a69a4e38d33a99fd49183972712d72133ab289d
PrathmeshModhe9 <lastry2001@gmail.com>
2023-06-24 20:21:29 +0530

    added firebase realtime presence for online/offline presence of user

--- src/components/dashboard/DashBoardToggle.js
+++ src/components/dashboard/DashBoardToggle.js
@@ -2,7 +2,8 @@ import React, { useCallback } from 'react'
 import { Button,Drawer,Icon,Alert } from 'rsuite'
 import { useMediaQuery, useModalState } from '../../misc/custom-hooks'
 import  Dashboard  from '.';
-import { auth } from '../../misc/firebase';
+import { auth,database } from '../../misc/firebase';
+import { isOfflineForDatabase } from '../../contexts/profile.context';
 
 
 const DashBoardToggle = () => {
@@ -10,10 +11,14 @@ const DashBoardToggle = () => {
   const isMobile=useMediaQuery('(max-width:992px)');
 
   const onSignOut=useCallback(()=>{
-    console.log("inside")
-    auth.signOut();
-    Alert.info('Signed out from device',4000);
-    close();
+   // console.log("inside")
+    database.ref(`/status/${auth.currentUser.uid}`).set(isOfflineForDatabase).then(()=>{
+      auth.signOut();
+      Alert.info('Signed out from device',4000);
+      close();
+    }).catch(err=>{
+      Alert.error(err.message,4000);
+    })
   },[close])
   return (
     <>
